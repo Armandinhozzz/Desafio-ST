de8d03df042ae066299a639b867f72cda545023d
Armandinhozzz <armandomalol@Hotmail.com>
2025-04-06 12:02:09 -0300

    Nosso primeiro commit

---
+++ Desafio.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main(){
+    char[50] estado, carta, nomeDaCidade;
+    int populaçao, pontosTuristicos;
+    float area, pib;
+
+    printf("Digite seu Estado: /n");
+    scanf("%s", &estado);
+
+    printf("Digite o nome da cidade: /n");
+    scanf("%s", &nomeDaCidade);
+    
+    printf("Digite o seu nome: /n");
+    scanf("%s", &nome);
+
+    printf("Digite sua matricula: /n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matricula: %d", nome, matricula);
+
+    printf("Idade:  %d - Altura:  %f", idade, altura);
+
+    return 0;
+    
+    }
